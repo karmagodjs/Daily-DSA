1bf3ef4ed9175307a59ab3c5bff67b8b72cf31c2
Dhruv Kumar <70892642+karmagodjs@users.noreply.github.com>
2026-04-20 06:39:14 +0530

    Time: 43 ms (24.7%), Space: 14.1 MB (81.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,20 +4,18 @@ using namespace std;
 class Solution {
 public:
 vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> mp;  // value -> index
-
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-
-        // check if complement exists
-        if (mp.find(complement) != mp.end()) {
-            return {mp[complement], i};
+        
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
         }
-
-        // store current element
-        mp[nums[i]] = i;
+        
+        return {};
     }
-
-    return {}; // no solution
-}
 };
+
+// O(n²) time complexity
